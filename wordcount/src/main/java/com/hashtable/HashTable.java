a2ed52d250ec4768004878dc91f753df85192c3a
Adarsh999bh <adarshbhandary48@gmail.com>
2021-09-09 17:00:00 +0530

    put method added to Hashtable.java and read content from file and created words array

--- wordcount/src/main/java/com/hashtable/HashTable.java
+++ wordcount/src/main/java/com/hashtable/HashTable.java
@@ -18,4 +18,46 @@ public class HashTable<K,D> {
         }
         return head;
     }
+    public int findHashValue(K key){
+        String stringKey= String.valueOf(key);
+        int sum=0;
+        for(int i=0;i<stringKey.length();i++){
+            sum+=stringKey.charAt(i);
+        }
+        return sum % this.sizeOfHashTable;
+    }
+    public Node<K,D> getNode(int index){
+        Node<K,D> temp=this.startingNode;
+        while(index>=0){
+            temp=temp.nextBlockLink;
+            index--;
+        }
+        return temp;
+    }
+    public void checkIfKeyIsAssignedElseCreatenewNode(Node<K,D> temp,K key,D data){
+        while(temp.chainingLink!=null){
+            if(temp.key.equals(key)){
+                System.out.println("Key is already assigned");
+                return;
+            }
+            temp=temp.chainingLink;
+        }
+        temp.chainingLink=new Node<>();
+        temp.chainingLink.data=data;
+        temp.chainingLink.key=key;
+        this.noOfElementsFilledHashtable+=1;
+    }
+    public void put(K key,D data){
+        if(key != null && data != null){
+            Node<K,D> temp=getNode(findHashValue(key));
+            if(temp.key==null){
+                temp.key=key;
+                temp.data=data;
+                this.noOfElementsFilledHashtable+=1;
+            }
+            else{
+                checkIfKeyIsAssignedElseCreatenewNode(temp,key,data);
+            }
+        }
+    }
 }
--- wordcount/src/main/java/com/hashtable/WordcountApp.java
+++ wordcount/src/main/java/com/hashtable/WordcountApp.java
@@ -1,13 +1,28 @@
 package com.hashtable;
 
+import java.io.*;
 /**
  * Hello world!
  *
  */
+import java.util.Scanner;
+
 public class WordcountApp 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to wordcount program.!" );
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter path to your file");
+        sc.close();
+        BufferedReader br=new BufferedReader(new FileReader(sc.nextLine()));
+        String line=null;
+        String content="";
+        while((line=br.readLine())!=null){
+            content+=line;
+        }
+        br.close();
+        String[] wordsArray=content.toLowerCase().split(" ");
+        
     }
 }
