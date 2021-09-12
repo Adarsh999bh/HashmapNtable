c579cfebe1f56e1cb068318337282bdeee5eb3c3
Adarsh999bh <adarshbhandary48@gmail.com>
2021-09-13 00:20:31 +0530

    function body added and code is complete

--- wordcount/src/main/java/com/hashtable/BinarySearchTree.java
+++ wordcount/src/main/java/com/hashtable/BinarySearchTree.java
@@ -1,24 +1,79 @@
 package com.hashtable;
 
-public class BinarySearchTree<T> {
-    BstNode<T> root;
+public class BinarySearchTree {
+    BstNode<Integer> root;
     public BinarySearchTree(){
-        root=null;
+        this.root=null;
     }
-    public void addNode(T data){
+    public BstNode<Integer> addNodedata(BstNode<Integer> root, Integer data){
+        if(root==null){
+            BstNode<Integer> temp=new BstNode<>();
+            temp.data=data;
+            return temp;
+        }
+        if(root.data.compareTo(data)>0){
+            root.left=addNodedata(root.left, data);
+            return root;
+        }
+        else if(root.data.compareTo(data)<0){
+            root.right=addNodedata(root.right, data);
+            return root;
+        }
+        else{
+            System.out.println("Redundant elements not allowed");
+            return root;
+        }
+    }
+    public void addNode(Integer data){
+        if(this.root==null){
+            root=new BstNode<>();
+            root.data=data;
+        }
+        else{
+            root=addNodedata(this.root,data);
+        }
+    }
+    public void inOrder(BstNode<Integer> root){
+        if(root==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.data+" ");
+        inOrder(root.right);
 
     }
-    public void inOrder(){
-
+    public void preOrder(BstNode<Integer> root){
+        if(root==null){
+            return;
+        }
+        System.out.println(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
     }
-    public void preOrder(){
-
+    public void postOrder(BstNode<Integer> root){
+        if(root==null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.data+" ");
     }
-    public void postOrder(){
-
-    }
-    public void search(T data){
-        
+    public void search(Integer data,BstNode<Integer> root){
+        if(root==null){
+            System.out.println("not found");
+            return;
+        }
+        if(root.data==data){
+            System.out.println("found");
+        }
+        else{
+            if(root.data>data){
+                search(data, root.left);
+            }
+            else{
+                search(data, root.right);
+            }
+        }
     }
     
 }
--- wordcount/src/main/java/com/hashtable/BstApp.java
+++ wordcount/src/main/java/com/hashtable/BstApp.java
@@ -1,8 +1,41 @@
 package com.hashtable;
-
+import java.util.Scanner;
 public class BstApp {
+    static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Welcome to Binary Search Tree");
-        
+        BinarySearchTree bst=new BinarySearchTree();
+        while(true){
+            System.out.println("Enter your choice \n1. Add Node\n2. Search Node\n3. Inorder\n4. Preorder\n5. Postorder");
+            int choice=Integer.parseInt(sc.nextLine());
+            switch(choice){
+                case 1:{
+                    System.out.println("Enter the number");
+                    bst.addNode(Integer.parseInt(sc.nextLine()));
+                    break;
+                }
+                case 2:{
+                    System.out.println("Enter the number you want to search");
+                    bst.search(Integer.parseInt(sc.nextLine()), bst.root);
+                    break;
+                }
+                case 3:{
+                    bst.inOrder(bst.root);
+                    break;
+                }
+                case 4:{
+                    bst.preOrder(bst.root);
+                    break;
+                }
+                case 5:{
+                    bst.postOrder(bst.root);
+                    break;
+                }
+                default:{
+                    sc.close();
+                    System.exit(0);
+                }
+            }
+        }
     }
 }
